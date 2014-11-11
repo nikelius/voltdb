32204201c655046aef078f8d80ddd896fc4add87
jcrawford <jcrawford@voltdb.com>
2014-11-11 11:27:56 -0500

    An initial cut at a new organization for the old "GEB studioWeb" tests, now in geb/vmc (VoltDB Management Center, that is). Lots more work to do here, but it's a start.

---
+++ tests/geb/vmc/src/test/groovy/RunQueriesInVMC.groovy
@@ -0,0 +1,73 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2014 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+//package vmcTest.test.groovy
+package vmcTest
+
+import geb.*
+import geb.spock.*
+import geb.driver.CachingDriverFactory
+import org.openqa.selenium.interactions.Actions
+import spock.lang.*
+import groovy.json.*
+//import vmcTest.SqlQueryPage
+//import vmcTest.VoltDBManagementCenterPage
+
+/*
+ * This class contains some initial tests....
+ */
+class RunQueriesInVMC extends GebReportingSpec {
+    
+    def myFirstTest() {
+        setup: 'Open VMC page'
+        to VoltDBManagementCenterPage
+
+        expect: 'to be on VMC page'
+        at VoltDBManagementCenterPage
+
+        // Go to (& use) SQL Query tab
+        //def sqp = page.openSqlQueryPage()
+        page.openSqlQueryPage()
+        def sqp = (SqlQueryPage) page
+        sqp.setQueryText('Foo Bar')
+        Thread.sleep(5 * 1000)
+        //sqp.clearQuery()
+        sqp.runQuery('select * from replicated_table')
+        Thread.sleep(5 * 1000)
+        assert sqp.isSqlQueryPageOpen()
+
+        // Go to Schema tab
+        //def scp = sqp.openSchemaPage()
+        page.openSchemaPage()
+        def scp = (SchemaPage) page
+        Thread.sleep(5 * 1000)
+        assert scp.isSchemaPageOpen()
+
+        // Go to DB Monitor tab
+        //def dbm = scp.openDbMonitorPage()
+        page.openDbMonitorPage()
+        def dbm = (DbMonitorPage) page
+        Thread.sleep(5 * 1000)
+        assert dbm.isDbMonitorPageOpen()
+    }
+}
---
+++ tests/geb/vmc/src/test/groovy/VoltDBManagementCenterPage.groovy
@@ -0,0 +1,116 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2014 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+//package vmcTest.pages
+package vmcTest
+
+import geb.*
+import geb.spock.*
+import geb.driver.CachingDriverFactory
+import org.openqa.selenium.interactions.Actions
+import spock.lang.*
+import groovy.json.*
+//import vmcTest.SqlQueryPage
+
+/*
+ * This class represents a generic VoltDB Management Center page (without
+ * specifying which tab you are on), which is the main page of the web UI
+ * - the new UI for version 4.9 (November 2014), replacing the old Web Studio
+ * (& DB Monitor Catalog Report, etc.).
+ */
+class VoltDBManagementCenterPage extends Page {
+    static url = 'http://localhost:8080/'
+    static content = {
+        navTabs { $('#nav') }
+        dbMonitorTab { navTabs.find('#navDbmonitor') }
+        schemaTab    { navTabs.find('#navSchema') }
+        sqlQueryTab  { navTabs.find('#navSqlQuery') }
+        dbMonitorLink(to: DbMonitorPage) { dbMonitorTab.find('a') }
+        schemaLink   (to: SchemaPage)    { schemaTab.find('a') }
+        sqlQueryLink (to: SqlQueryPage)  { sqlQueryTab.find('a') }
+    }
+    static at = {
+        title == 'VoltDB Management Center'
+        dbMonitorLink.displayed
+        schemaLink.displayed
+        sqlQueryLink.displayed
+    }
+    
+    def boolean isDbMonitorPageOpen() {
+        if (dbMonitorTab.attr('class') == 'active') {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    def boolean isSchemaPageOpen() {
+        if (schemaTab.attr('class') == 'active') {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    def boolean isSqlQueryPageOpen() {
+        if (sqlQueryTab.attr('class') == 'active') {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    // TODO: fix this!
+    def VoltDBManagementCenterPage getPage() {
+        if (isDbMonitorPageOpen()) {
+            return (DbMonitorPage) this
+        } else if(isSchemaPageOpen()) {
+            return (SchemaPage) this
+        } else if(isSqlQueryPageOpen()) {
+            return (SqlQueryPage) this
+        } else {
+            return null;
+        }
+    }
+    
+    def void openDbMonitorPage() {
+        if (!isDbMonitorPageOpen()) {
+            dbMonitorLink.click()
+        }
+        //return (DbMonitorPage) this
+    }
+    
+    def void openSchemaPage() {
+        if (!isSchemaPageOpen()) {
+            schemaLink.click()
+        }
+        //return (SchemaPage) this
+    }
+
+    def void openSqlQueryPage() {
+        if (!isSqlQueryPageOpen()) {
+            sqlQueryLink.click()
+        }
+        //return (SqlQueryPage) this
+    }
+}
