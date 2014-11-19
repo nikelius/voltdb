06e671b29ff391882951b60a23e9f53a5cd3f76d
jcrawford <jcrawford@voltdb.com>
2014-11-19 16:48:46 -0500

    RunQueriesInVmcTest: added a bunch of experimental tests, much of which should be reorganized or removed.

--- tests/geb/vmc/src/tests/RunQueriesInVmcTest.groovy
+++ tests/geb/vmc/src/tests/RunQueriesInVmcTest.groovy
@@ -31,12 +31,124 @@ import spock.lang.*
 import groovy.json.*
 import vmcTest.pages.*
 
-/*
+// TODO: temp debug:
+class EchoingPageChangeListener implements PageChangeListener {
+    void pageWillChange(Browser browser, Page oldPage, Page newPage) {
+        println "browser '$browser' changing page from '$oldPage' to '$newPage'"
+    }
+}
+
+/**
  * This class contains some initial tests....
  */
-class RunQueriesInVMC extends GebReportingSpec {
-    
+class RunQueriesInVMCTest extends GebReportingSpec {
+    // TODO: temp debug??
+    static sleepSecs = 1
+    static numRowsToInsert = 4
+/*
+    private def getSqlQueryPage() {
+        setup: 'Open VMC page'
+        to VoltDBManagementCenterPage
+
+        expect: 'to be on VMC page'
+        at VoltDBManagementCenterPage
+
+        // Go to (& use) SQL Query tab
+        page.openSqlQueryPage()
+        //return (SqlQueryPage) page
+    }
+*/
+    static def queryFrom(SqlQueryPage sqp, List<String> tables) {
+        tables.each {
+            String query = 'select * from ' + it + ' limit 10'
+            println "\nQuery:\n  " + query
+            sqp.runQuery(query)
+            def qResult = sqp.getQueryResult()
+            println "Result:"
+            println '  ' + qResult
+        }
+    }
+
+    static def deleteFrom(SqlQueryPage sqp, List<String> tables) {
+        tables.each {
+            String query = 'delete from ' + it
+            println "\nQuery:\n  " + query
+            sqp.runQuery(query)
+            def qResult = sqp.getQueryResult()
+            println "Result:"
+            println '  ' + qResult
+        }
+    }
+
+    static def insertInto(SqlQueryPage sqp, List<String> tables, int numToInsert) {
+        tables.each {
+            def columns = sqp.getTableColumns(it)
+            println "columns (" + it + "): " + columns
+            def count = 0
+            for (int i = 1; i <= numToInsert; i++) {
+                String query = "insert into " + it + " values ("
+                for (int j = 0; j < columns.size(); j++) {
+                    if (columns.get(j).contains('varchar')) {
+                        query += (j > 0 ? ", " : "") + "'a" + i + "'"
+                    } else {
+                        query += (j > 0 ? ", " : "") + i
+                    }
+                }
+                query += ")"
+                println "\nQuery:\n  " + query
+                sqp.runQuery(query)
+                def qResult = sqp.getQueryResult()
+                println "Result:"
+                println '  ' + qResult
+            }
+        }
+    }
+
+    def insertIntoAllTables() {
+        setup: 'Open VMC page'
+        to VoltDBManagementCenterPage
+        expect: 'to be on VMC page'
+        at VoltDBManagementCenterPage
+        page.openSqlQueryPage()
+        def sqp = (SqlQueryPage) page
+        assert sqp.isSqlQueryPageOpen()
+        
+        def tables = sqp.getTableNames()
+        println "\nTables:\n  " + tables
+        deleteFrom(sqp, tables)
+        insertInto(sqp, tables, numRowsToInsert)
+    }
+
+    def queryAllTablesAndViews() {
+        setup: 'Open VMC page'
+        to VoltDBManagementCenterPage
+        expect: 'to be on VMC page'
+        at VoltDBManagementCenterPage
+        page.openSqlQueryPage()
+        def sqp = (SqlQueryPage) page
+        assert sqp.isSqlQueryPageOpen()
+        
+        def tables = sqp.getTableNames()
+        println "\nTables:\n  " + tables
+        tables.each { println "\nColumns (" + it + "): " + sqp.getTableColumns(it) }
+        tables.each { println "\nColumn names (" + it + "): " + sqp.getTableColumnNames(it) }
+        tables.each { println "\nColumn types (" + it + "): " + sqp.getTableColumnTypes(it) }
+        queryFrom(sqp, tables)
+        
+        def views = sqp.getViewNames()
+        println "\nViews:\n  " + views
+        views.each { println "\nColumns (" + it + "): " + sqp.getViewColumns(it) }
+        views.each { println "\nColumn names (" + it + "): " + sqp.getViewColumnNames(it) }
+        views.each { println "\nColumn types (" + it + "): " + sqp.getViewColumnTypes(it) }
+        queryFrom(sqp, views)
+    }
+
     def myFirstTest() {
+        // TODO: temp debug:
+        def listener = new EchoingPageChangeListener()
+        browser.registerPageChangeListener(listener)
+//        println "geb.env: " + geb.env
+
         setup: 'Open VMC page'
         to VoltDBManagementCenterPage
 
@@ -47,25 +159,96 @@ class RunQueriesInVMC extends GebReportingSpec {
         //def sqp = page.openSqlQueryPage()
         page.openSqlQueryPage()
         def sqp = (SqlQueryPage) page
-        sqp.setQueryText('Foo Bar')
-        Thread.sleep(5 * 1000)
+        //sqp.setQueryText('select * from partitioned_table')
+        sqp.runQuery('select * from contestants')
+        Thread.sleep(sleepSecs * 1000)
         //sqp.clearQuery()
-        sqp.runQuery('select * from replicated_table')
-        Thread.sleep(5 * 1000)
+        //sqp.runQuery('select * from replicated_table')
+        sqp.runQuery('select * from votes limit 20')
+        Thread.sleep(sleepSecs * 1000)
         assert sqp.isSqlQueryPageOpen()
 
         // Go to Schema tab
         //def scp = sqp.openSchemaPage()
         page.openSchemaPage()
         def scp = (SchemaPage) page
-        Thread.sleep(5 * 1000)
+        Thread.sleep(sleepSecs * 1000)
         assert scp.isSchemaPageOpen()
 
         // Go to DB Monitor tab
         //def dbm = scp.openDbMonitorPage()
         page.openDbMonitorPage()
         def dbm = (DbMonitorPage) page
-        Thread.sleep(5 * 1000)
+        Thread.sleep(sleepSecs * 1000)
         assert dbm.isDbMonitorPageOpen()
     }
+
+    def printTableViewAndStoredProcInfo() {
+        setup: 'Open VMC page'
+        to VoltDBManagementCenterPage
+        expect: 'to be on VMC page'
+        at VoltDBManagementCenterPage
+        page.openSqlQueryPage()
+        def sqp = (SqlQueryPage) page
+
+        def tables = sqp.getTableNames()
+        println "\nTable Names:\n" + tables
+        def views = sqp.getViewNames()
+        println "\nView Names:\n" + views
+        def storedProcs = sqp.getStoredProcedureNames()
+        println "\nStoredProcedure Names:\n" + storedProcs
+    }
+
+    def printQueryResults() {
+
+        setup: 'Open VMC page'
+        to VoltDBManagementCenterPage
+
+        expect: 'to be on VMC page'
+        at VoltDBManagementCenterPage
+
+        // Go to (& use) SQL Query tab
+        //def sqp = page.openSqlQueryPage()
+        page.openSqlQueryPage()
+        def sqp = (SqlQueryPage) page
+        sqp.runQuery("upsert into contestants values (1, 'John Smith')")
+        Thread.sleep(sleepSecs * 1000)
+        sqp.runQuery("upsert into contestants values (2, 'Mary Jones')")
+        Thread.sleep(sleepSecs * 1000)
+        sqp.runQuery('select * from contestants')
+        Thread.sleep(sleepSecs * 1000)
+        def qRes = sqp.getQueryResult(0)
+        println "\nQuery Result:\n" + qRes
+        sqp.runQuery('select count(*) from votes;\nselect * from contestants')
+        Thread.sleep(sleepSecs * 1000)
+        def qResults = sqp.getQueryResults()
+        println "\nQuery Results (" + qResults.size() + "):"
+        qResults.each { println it }
+        def lastRes = sqp.getQueryResult()
+        println "\nLast Query Result:\n" + lastRes
+    }
+//*
+    def printSomeValues() {
+        setup: 'Open VMC page'
+        to VoltDBManagementCenterPage
+        println "\npage: " + page
+        println "page.getProperties(): " + page.getProperties()
+        println "page.content.getProperties(): " + page.content.getProperties()
+        
+        page.openSqlQueryPage()
+        println "page: " + page
+        println "page.getProperties(): " + page.getProperties()
+        println "page.content.getProperties(): " + page.content.getProperties()
+        
+        page.openSchemaPage()
+        println "page: " + page
+        println "page.getProperties(): " + page.getProperties()
+        println "page.content.getProperties(): " + page.content.getProperties()
+        
+        page.openDbMonitorPage()
+        println "page: " + page
+        println "page.getProperties(): " + page.getProperties()
+        println "page.content.getProperties(): " + page.content.getProperties()
+    }
+//*/
 }
